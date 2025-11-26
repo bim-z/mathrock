7df6dd5b0ffa405e5df2bbd193298d69d7d71f81
simpleajaa <kerjalikeanastronaut@gmail.com>
2025-11-26 02:57:36 +0000

    add implementation if locking to save handler

--- cmd/save.go
+++ cmd/save.go
@@ -5,5 +5,6 @@ import "github.com/spf13/cobra"
 var save = &cobra.Command{
 	Use:   "save",
 	Short: "",
-	RunE:  func(cmd *cobra.Command, args []string) error {},
+	RunE: func(cmd *cobra.Command, args []string) error {
+	},
 }
