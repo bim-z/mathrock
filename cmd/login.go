4b8315bcb078bd7b7ec6e35a54ff55f5502a55fe
simpleajaa <kerjalikeanastronaut@gmail.com>
2025-11-29 03:01:16 +0000

    create login method and add description into it

--- cmd/login.go
+++ cmd/login.go
@@ -4,6 +4,6 @@ import "github.com/spf13/cobra"
 
 var login = &cobra.Command{
 	Use:   "login",
-	Short: "",
+	Short: "Log in to your account",
 	RunE:  func(cmd *cobra.Command, args []string) error {},
 }
